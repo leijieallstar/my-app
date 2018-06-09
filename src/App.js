5ee09f45bca892d26758f468c10c3eca92d0ea05
leijie <2929041415@qq.com>
2018-06-09 13:49:09 +0800

    初始化项目 学习用

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import MainHeader from './component/include/MainHeader'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <MainHeader/>
+      </div>
+    );
+  }
+}
+
+export default App;
